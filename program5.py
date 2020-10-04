739291b06b71a6b749a19774f274a80efb484619
paras283 <72180143+paras283@users.noreply.github.com>
2020-10-04 14:28:45 +0530

    Update program5.py
    Bhut bda nm de diya variable ka... Isliye mne ise 6ota kr diya

--- program5.py
+++ program5.py
@@ -8,5 +8,5 @@ print("Enter the elements in List2 one by one")
 list2 = []
 for i in range(size2):
     list2.append(input())
-intersectionList = list(set(list1).intersection(set(list2)))
+interList = list(set(list1).intersection(set(list2)))
 print("The intersection of List 1 and List 2 is:", ", ".join(intersectionList))
